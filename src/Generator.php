bf5d753522ea9d8dd994f6c00e4838bfc26d2c96
Vasyl Varynkiv <varynkiv@gmail.com>
2018-07-18 22:44:32 +0300

    update generator

--- src/Generator.php
+++ src/Generator.php
@@ -8,29 +8,32 @@ class Generator
 {
     public static function generateDocs()
     {
-        $appDir = config('swagger-lume.paths.annotations');
-        $docDir = config('swagger-lume.paths.docs');
-        if (! File::exists($docDir) || is_writable($docDir)) {
-            // delete all existing documentation
-            if (File::exists($docDir)) {
-                File::deleteDirectory($docDir);
+        $version = config('swagger-lume.version');
+        foreach ($version as $key => $val) {
+            $appDir = config('swagger-lume.paths.annotations') . '/' . $val;
+            $docDir = config('swagger-lume.paths.docs') . '/' . $val;
+            if (!File::exists($docDir) || is_writable($docDir)) {
+                // delete all existing documentation
+                if (File::exists($docDir)) {
+                    File::deleteDirectory($docDir);
+                }
+
+                self::defineConstants(config('swagger-lume.constants') ?: []);
+
+                File::makeDirectory($docDir);
+                $excludeDirs = config('swagger-lume.paths.excludes');
+                $swagger = \Swagger\scan($appDir, ['exclude' => $excludeDirs]);
+
+                if (config('swagger-lume.paths.base') !== null) {
+                    $swagger->basePath = config('swagger-lume.paths.base');
+                }
+
+                $filename = $docDir . '/' . config('swagger-lume.paths.docs_json');
+                $swagger->saveAs($filename);
+
+                $security = new SecurityDefinitions();
+                $security->generate($filename);
             }
-
-            self::defineConstants(config('swagger-lume.constants') ?: []);
-
-            File::makeDirectory($docDir);
-            $excludeDirs = config('swagger-lume.paths.excludes');
-            $swagger = \Swagger\scan($appDir, ['exclude' => $excludeDirs]);
-
-            if (config('swagger-lume.paths.base') !== null) {
-                $swagger->basePath = config('swagger-lume.paths.base');
-            }
-
-            $filename = $docDir.'/'.config('swagger-lume.paths.docs_json');
-            $swagger->saveAs($filename);
-
-            $security = new SecurityDefinitions();
-            $security->generate($filename);
         }
     }
 
